2112bfc8f3986befcd7b3b3800a64874a28f9080
Eusebiu Antoniu <wyyph256wp@privaterelay.appleid.com>
2025-12-31 07:50:01 +0100

    add Charmeleon to pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
     { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
     { name: 'Butterfree', height: 11, types: ['bug', 'flying'] },
-    { name: 'Pikachu', height: 4, types: ['electric'] }
+    { name: 'Pikachu', height: 4, types: ['electric'] },
+    { name: 'Charmeleon', height: 11, types: ['fire'] }
 ];
