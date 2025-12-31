6b3789dae8501d0a6ad7f4f74450b78cb663ddba
Eusebiu Antoniu <wyyph256wp@privaterelay.appleid.com>
2025-12-31 07:51:56 +0100

    add Charizard to pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ let pokemonList = [
     { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
     { name: 'Butterfree', height: 11, types: ['bug', 'flying'] },
     { name: 'Pikachu', height: 4, types: ['electric'] },
-    { name: 'Charmeleon', height: 11, types: ['fire'] }
+    { name: 'Charmeleon', height: 11, types: ['fire'] },
+    { name: 'Charizard', height: 17, types: ['fire', 'flying'] }
 ];
