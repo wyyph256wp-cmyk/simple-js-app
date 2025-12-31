bbf7f87506c999ba779110a37d60324b289eafff
Eusebiu Antoniu <wyyph256wp@privaterelay.appleid.com>
2025-12-31 07:46:04 +0100

    add Butterfree to pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] }
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+    { name: 'Butterfree', height: 11, types: ['bug', 'flying'] }
 ];
