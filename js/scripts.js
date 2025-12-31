bc5f2bd18731fe282b94ccad51bc6528c53f3618
Eusebiu Antoniu <wyyph256wp@privaterelay.appleid.com>
2025-12-31 07:48:03 +0100

    add Pikachu to pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonList = [
     { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { name: 'Butterfree', height: 11, types: ['bug', 'flying'] }
+    { name: 'Butterfree', height: 11, types: ['bug', 'flying'] },
+    { name: 'Pikachu', height: 4, types: ['electric'] }
 ];
