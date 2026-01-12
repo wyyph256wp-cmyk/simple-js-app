13553b601b742e7438aa4d4f54f3934fa874c321
Eusebiu Antoniu <wyyph256wp@privaterelay.appleid.com>
2026-01-12 22:38:56 +0100

    add conditional to highlight big pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,12 @@ let pokemonList = [
     { name: 'Charmeleon', height: 11, types: ['fire'] },
     { name: 'Charizard', height: 17, types: ['fire', 'flying'] }
 ];
+// Display each Pokémon's name and height on the webpage
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+    // Check if the Pokémon's height is greater than 15
+    if (pokemonList[i].height > 15) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '</p>');
+    } else
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+
 }
