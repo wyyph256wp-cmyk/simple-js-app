50bcc7b5e5f89d376e251e7f1c02a2acc0170c71
Eusebiu Antoniu <wyyph256wp@privaterelay.appleid.com>
2026-01-26 20:02:18 +0100

    replace for loop with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,22 @@ let pokemonList = [
     { name: 'Charmeleon', height: 11, types: ['fire'] },
     { name: 'Charizard', height: 17, types: ['fire', 'flying'] }
 ];
-// Display each Pokémon's name and height on the webpage
-for (let i = 0; i < pokemonList.length; i++) {
-    // Check if the Pokémon's height is greater than 15
-    if (pokemonList[i].height > 15) {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '</p>');
-    } else
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
 
-}
+// Display each Pokémon's name and height on the webpage using for loop
+// for (let i = 0; i < pokemonList.length; i++) {
+//     // Check if the Pokémon's height is greater than 15
+//     if (pokemonList[i].height > 15) {
+//         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '</p>');
+//     } else
+//         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+// }
+
+// Display each Pokémon's name and height on the webpage using forEach
+pokemonList.forEach(function (pokemon) {
+    // Check if the Pokémon's height is greater than 15
+    if (pokemon.height > 15) {
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + ' - Wow, that\'s big!' + '</p>');
+    } else {
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + '</p>');
+    }
+}); 
