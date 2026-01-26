af85ff3ddc51c53bd4ec15441692720c636267d1
Eusebiu Antoniu <wyyph256wp@privaterelay.appleid.com>
2026-01-26 20:33:29 +0100

    wrap pokemonList in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { name: 'Butterfree', height: 11, types: ['bug', 'flying'] },
-    { name: 'Pikachu', height: 4, types: ['electric'] },
-    { name: 'Charmeleon', height: 11, types: ['fire'] },
-    { name: 'Charizard', height: 17, types: ['fire', 'flying'] }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+        { name: 'Butterfree', height: 11, types: ['bug', 'flying'] },
+        { name: 'Pikachu', height: 4, types: ['electric'] },
+        { name: 'Charmeleon', height: 11, types: ['fire'] },
+        { name: 'Charizard', height: 17, types: ['fire', 'flying'] }
+    ];
+    function getAll() {
+        return pokemonList;
+    }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
 // Display each Pokémon's name and height on the webpage using for loop
-// for (let i = 0; i < pokemonList.length; i++) {
+// for (let i = 0; i < pokemonRepository.getAll().length; i++) {
 //     // Check if the Pokémon's height is greater than 15
-//     if (pokemonList[i].height > 15) {
-//         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '</p>');
+//     if (pokemonRepository.getAll()[i].height > 15) {
+//         document.write('<p>' + pokemonRepository.getAll()[i].name + ' (height: ' + pokemonRepository.getAll()[i].height + ')' + ' - Wow, that\'s big!' + '</p>');
 //     } else
-//         document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+//         document.write('<p>' + pokemonRepository.getAll()[i].name + ' (height: ' + pokemonRepository.getAll()[i].height + ')' + '</p>');
 // }
 
 // Display each Pokémon's name and height on the webpage using forEach
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
     // Check if the Pokémon's height is greater than 15
     if (pokemon.height > 15) {
         document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + ' - Wow, that\'s big!' + '</p>');
